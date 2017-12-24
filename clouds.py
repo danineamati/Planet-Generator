89a518451a4036a22edc6ae245a4f2b132192531
Joseph <jojorocketn@gmail.com>
2017-12-24 14:41:10 -0500

    Changed colors of clouds. Added some color variety to clouds.

--- clouds.py
+++ clouds.py
@@ -16,7 +16,7 @@ class Clouds:
         self.rect_width = planet.radius / 1.5
         self.rect_height = planet.radius / 5
         self.endcirc_radius = self.rect_height / 2
-        self.color = "#CFD8DC"
+        self.color = ["#F7FAFA", "#F0F5F5", "#E8F0F0", "#E0EBEB"]
         self.max_cloud_lat = 0.75 * planet.radius
         self.min_cloud_lat = -(0.75 * planet.radius)
         
@@ -30,17 +30,19 @@ class Clouds:
         
         x = random.randrange(-(int(x_para)), int(x_para + 1)) + self.planet_x
         y = delta_y + self.planet_y
+
+        color = random.choice(self.color)
         
         self.canvas.create_rectangle((x - self.rect_width / 2),\
                                 (y - self.rect_height / 2),\
                                 (x + self.rect_width / 2),\
-                                (y + self.rect_height / 2), fill = self.color,\
-                                outline = self.color)
+                                (y + self.rect_height / 2), fill = color,\
+                                outline = color)
         
         drawCircle(x + self.rect_width / 2, y, self.endcirc_radius,\
-                        self.color, self.canvas)
+                        color, self.canvas)
         drawCircle(x - self.rect_width / 2, y, self.endcirc_radius,\
-                        self.color, self.canvas)
+                        color, self.canvas)
         
     def manyClouds(self):
         ''' Draws many clouds.'''
