8b7a0a9cd31ce700e44879bad9fb6440ee77958d
Joseph <jojorocketn@gmail.com>
2017-12-24 12:24:28 -0500

    Added more to drawClouds and actually drew 1 cloud in center of canvas.

--- planet_generator.py
+++ planet_generator.py
@@ -62,14 +62,17 @@ class Planets:
                                        
 class Clouds:
     ''' This class generates the atmosphere of the planet.'''
-    def __init__(self, planet):
+    def __init__(self, planet, planet_x, planet_y):
         self.rect_width = planet.radius
         self.rect_height = planet.radius / 5
         self.endcirc_radius = self.rect_height / 2
         self.color = "#F8F9F9"
+        self.max_cloud_lat = 0.75 * planet.radius
+        self.min_cloud_lat = -(0.75 * planet.radius)
         
     def drawClouds(self, x, y):
-        create.create_rectangle((x - self.rect_width / 2),\
+        ''' Draws the clouds onscreen.'''
+        canvas.create_rectangle((x - self.rect_width / 2),\
                                 (y - self.rect_height / 2),\
                                 (x + self.rect_width / 2),\
                                 (y + self.rect_height / 2), fill = self.color,\
@@ -165,6 +168,9 @@ if __name__ == '__main__':
     planet1 = Planets('Yavin5')
     writePlanetSpecs(planet1)
     drawPlanet(planet_x, planet_y, planet1)
+
+    cloud = Clouds(planet1, planet_x, planet_y)
+    cloud.drawClouds(500, 350)
 
     #print(font.families())
     root.bind('<q>', quit)
